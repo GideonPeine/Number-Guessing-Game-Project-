04f770a93f5c660a252650b0479f552d7f94fc5d
AidanReam <aidan.ream2009@gmail.com>
2023-11-03 14:17:01 -0500

    Update Number-Guessing-Game-Project-.py

--- Number-Guessing-Game-Project-.py
+++ Number-Guessing-Game-Project-.py
@@ -4,5 +4,15 @@ def num_guess():
     #num_guess accepts no arguments
     #determines higher or lower
     #ouputs if they guess it or not
+    minimum = int(input('Enter the minimum: '))
+    maximum = int(input('Enter the maxiumum: '))
+    
+    #sets variables if nothing is entered
+    if minimum == 0 or minimum == '':
+        minimum = 1
+    if maximum == 0 or maximum == '':
+        maximum = 1000
+    print (minimum, maximum)
+        
     
     
