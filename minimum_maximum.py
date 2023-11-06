7f5b9745ae93bdee519279d73c6c555fbfdc0e30
AidanReam <aidan.ream2009@gmail.com>
2023-11-06 14:08:44 -0600

    minimum_maximum_complete

---
+++ minimum_maximum.py
@@ -0,0 +1,38 @@
+#number guessing game
+
+def minimum():
+    #num_guess accepts no arguments
+    #determines minimum and maximum
+    #returns minimum and maximum 
+    
+    #grabs variables from users
+    minimum = int(input('Enter the minimum: '))
+    
+    
+    #sets variables if nothing is entered
+    if minimum == 0:
+        minimum = 1
+        
+    #returns value
+    return minimum
+
+def maximum():
+    #maximum accepts no arguments
+    #prompts user for maximum
+    #returns maximum
+    
+    #grabs variables from users
+    maximum = int(input('Enter the maxiumum: '))
+    
+    #sets variables if nothing is entered
+    if maximum == 0:
+        maximum = 1000
+        
+    #returns value
+    return maximum 
+        
+
+        
+        
+        
+#test
