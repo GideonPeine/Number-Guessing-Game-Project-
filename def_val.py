9c9bd83da10d64b562291567427b0740113fc413
AidanReam <aidan.ream2009@gmail.com>
2023-11-09 14:50:44 -0600

    menus added

--- def_val.py
+++ def_val.py
@@ -1,12 +1,15 @@
+import controller
+
 def def_val():
     #finds minimum and maximum
     mini = int(input('Enter the minimum: '))
     maxi='b'
     while maxi=='b' or maxi< mini:
         maxi = int(input('Enter the maxiumum: '))
-    
+    controller.cont()
     #sets variables if nothing is entered
     minmax=[mini, maxi]
+  
     return(minmax)
         
     
--- player_names.py
+++ player_names.py
@@ -1,3 +1,5 @@
+import controller
+
 def player_amount():
     #player_amount accepts no arguments
     #prompts user for player amount
@@ -5,11 +7,12 @@ def player_amount():
     amount=-1
     while amount<1:
         amount = int(input('Enter amount of players: '))
-    
     return amount
+
 def player_names():
     #player_names accepts amount
     #prompts users for there names
     #returns players names
-        name = (input('Enter name of player: '))
-        return name
+    name = (input('Enter name of player: '))
+    controller.cont()
+    return name
